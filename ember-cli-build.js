09ab4073a1e7050d531fb2c91c78630a3df72be4
Will Engler <Engler.Will@gmail.com>
2016-02-10 12:57:51 -0600

    Add bootstrap and original Plenario custom styling

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,6 +11,8 @@ module.exports = function(defaults) {
   app.import('bower_components/leaflet/dist/leaflet.css');
   app.import('bower_components/leaflet-draw/dist/leaflet.draw.js');
   app.import('bower_components/leaflet-draw/dist/leaflet.draw.css');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
