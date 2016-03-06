074124f70e6c14e906b661f9a09c993cbde27891
Will Engler <Engler.Will@gmail.com>
2016-03-06 11:22:40 -0600

    Got a chart in a table

---
+++ tests/unit/utils/prep-timeseries-test.js
@@ -0,0 +1,10 @@
+import prepTimeseries from 'ember-plenario/utils/prep-timeseries';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | prep timeseries');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = prepTimeseries();
+  assert.ok(result);
+});
