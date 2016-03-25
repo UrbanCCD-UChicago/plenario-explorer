40c1eaaef7834ccdfb1104f35acfbe0739d24903
Will Engler <Engler.Will@gmail.com>
2016-03-25 12:50:49 +0100

    Move timeseries munging to the serializer

--- app/serializers/timeseries.js
+++ app/serializers/timeseries.js
@@ -1,4 +1,6 @@
 import DS from 'ember-data';
+import moment from 'moment';
+
 
 //Timeseries endpoint has structure of
 /*{
@@ -21,16 +23,33 @@ import DS from 'ember-data';
 
 export default DS.JSONSerializer.extend({
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    var doc = {
+    //console.log(payload);
+    return {
       "data": {
         "type": "timeseries",
         "id": id,
         "attributes": {
-          "series": payload.objects
+          "series": this.prepTimeseries(payload.objects)
         }
       }
     };
-
-    return doc;
-  }
+  },
+  prepTimeseries(ts) {
+  /*
+   Takes array of objects of the form [{count: 3, datetime: '2016-12-06'}...]
+   Returns array of arrays of the form [[momentJSObject, integer]]
+   */
+  const formattedSeries = ts.map(function(timeSlice) {
+    // Why exactly does `moment(timeSlice.datetime + "+0000").valueOf()` work
+    // to let Highcharts accept datetimes on the x axis?
+    // I don't know. Don't question it.
+    return [moment(timeSlice.datetime + "+0000").valueOf(), timeSlice.count];
+  });
+  // The chart expects a list of series objects,
+  // each with a data attribute that actually holds the timeseries.
+  // So construct a list of one such object.
+  // NB: name: 'Count' determines chart tooltip.
+  // Easy to override in application if desired
+  return [{data: formattedSeries, name: 'Count'}];
+}
 });
