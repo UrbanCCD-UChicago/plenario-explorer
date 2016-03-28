b90ad561c335c68292e2931249978b2f800e961c
Will Engler <Engler.Will@gmail.com>
2016-03-28 10:00:41 +0100

    Add glyphicons

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,10 +13,14 @@ module.exports = function(defaults) {
   app.import('bower_components/leaflet-draw/dist/leaflet.draw.css');
   app.import('bower_components/bootswatch/simplex/bootstrap.css');
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
+    destDir: 'fonts'
+  });
   app.import('bower_components/datatables.net/js/jquery.dataTables.js');
   app.import('bower_components/datatables.net-bs/js/dataTables.bootstrap.js');
   app.import('bower_components/datatables.net-bs/css/dataTables.bootstrap.css');
   app.import('bower_components/urijs/src/URI.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
