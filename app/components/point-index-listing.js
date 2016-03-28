bff3edf0fade0a8e844d5295626f8e8ef60fcdc8
Will Engler <Engler.Will@gmail.com>
2016-03-28 14:34:52 +0100

    Add table styling to point dataset index

--- app/components/point-index-listing.js
+++ app/components/point-index-listing.js
@@ -8,11 +8,24 @@ export default Ember.Component.extend({
     },
     {
       "title": "Dataset Name",
-      "template": "display-link"
+      "template": "display-link",
+      "sortedBy": "humanName"
     },
     {
       "title": "Time span available",
       "template": "time-span"
+    },
+    {
+      "title": "Name Plaintext",
+      "isHidden": true,
+      "propertyName": "humanName"
     }
-  ]
+  ],
+  customMessages: {
+    "searchLabel": "Filter on text:",
+    "tableSummary": "Showing %@ - %@ of %@",
+    "noDataToShow": "Could not find any data within your filters"
+  },
+  pageSizeValues: Ember.A([5, 10, 25, 50]),
+  customClasses: {"globalFilterWrapper": "pull-left bottom-breathe"}
 });
