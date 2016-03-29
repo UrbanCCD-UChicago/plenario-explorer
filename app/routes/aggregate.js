aa8ad13633ffd78437d89cb84ab30c1ea5a3801b
Will Engler <Engler.Will@gmail.com>
2016-03-29 12:06:04 +0100

    Add GeoJSON validation on route transition

--- app/routes/aggregate.js
+++ app/routes/aggregate.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
-import moment from 'moment';
+import GJV from "npm:geojson-validation";
 
 export default Ember.Route.extend({
+  notify: Ember.inject.service('notify'),
   // A change in any query parameter should trigger a refresh.
   queryParams: {
     obs_date__le: {refreshModel: true},
@@ -12,8 +13,44 @@ export default Ember.Route.extend({
   model(params) {
     return Ember.RSVP.hash({
       pointDatasets: this.store.query('pointDataset', params),
-      // Will need to change this to query.
       shapeDatasets: this.store.query('shapeDataset', params)
     });
+  },
+
+  /**
+   * A place to impose restrictions on the query parameters
+   * so that we can't enter aggregate with badly formatted params.
+   *
+   * @param transition
+   */
+  beforeModel(transition) {
+    let self = this;
+    let bailToIndex = function(message) {
+      self.transitionTo('index');
+      self.get('notify').error(message);
+    };
+
+    // Check that location_geom__within is a valid polygon or line segment
+    const genericHelp = 'Please draw a shape on the map before submitting.';
+
+    let geoJSON;
+    try {
+      let geoJSONStr = transition.queryParams.location_geom__within;
+      geoJSON = JSON.parse(geoJSONStr);
+    }
+    catch (err) {
+      bailToIndex('Invalid JSON. ' + genericHelp);
+    }
+
+    if (!GJV.isFeature(geoJSON)) {
+      bailToIndex('GeoJSON must be a "Feature" document. ' + genericHelp);
+    }
+    geoJSON = geoJSON.geometry;
+    let isPolygonOrLine = GJV.isPolygon(geoJSON) || GJV.isLineString(geoJSON);
+    if (!isPolygonOrLine) {
+      bailToIndex('Geometry must be a polygon or line. ' + genericHelp);
+    }
+
+    // Check that agg is valid
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  notify: Ember.inject.service('notify'),
   model() {
     return {
       pointDatasets: this.store.findAll('pointDataset'),
       shapeDatasets: this.store.findAll('shapeDataset')
     };
+  },
+  willTransition() {
+    
   }
+  //afterModel() {
+  //  this.get('notify').info('Hello there!');
+  //}
 });
