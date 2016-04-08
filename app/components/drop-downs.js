6e6589a7864ca70a331fa37b33559301dcc05992
Will Engler <Engler.Will@gmail.com>
2016-04-08 15:04:39 -0500

    Use Ember Rapid Forms for dropdowns

---
+++ app/components/drop-downs.js
@@ -0,0 +1,42 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  init() {
+    this._super(...arguments);
+    const agg = this.get('agg');
+    const res = this.get('res');
+    const optionState = Ember.Object.create({
+      agg: agg,
+      res: res
+    });
+    this.set('optionState', optionState);
+  },
+
+  aggOptions: ([
+    {id: 'day', label: 'day'},
+    {id: 'week', label: 'week'},
+    {id: 'month', label: 'month'},
+    {id: 'quarter', label: 'quarter'},
+    {id: 'year', label: 'year'}
+  ]),
+  
+  resOptions: ([
+    {id: '100', label: '100 meters'},
+    {id: '200', label: '200 meters'},
+    {id: '300', label: '300 meters'},
+    {id: '400', label: '400 meters'},
+    {id: '500', label: '500 meters'},
+    {id: '1000', label: '1 kilometer'}
+  ]),
+
+  changedAgg: Ember.observer('optionState.agg', function() {
+    const newAgg = this.get('optionState.agg');
+    this.set('agg', newAgg);
+  }),
+
+  changedRes: Ember.observer('optionState.res', function() {
+    const newRes = this.get('optionState.res');
+    this.set('res', newRes);
+  })
+
+});
