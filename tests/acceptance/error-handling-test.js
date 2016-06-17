df18c6c95948c8ffd7a3adec32e6c72b9b09bc99
George Moe <george@george.moe>
2016-06-17 12:03:47 -0500

    Added error handling for malformed URIs.

--- tests/acceptance/error-handling-test.js
+++ tests/acceptance/error-handling-test.js
@@ -107,7 +107,7 @@ test('Attempting to query without a geoJSON returns to index and issues an error
 });
 
 /*test('Attempting to access an invalid URI returns to index.', function(assert){
-  visit('/discover/aggregate?location_geom__within=%7B%22type%22%3%2ture%22%3A%7B%7D%2C%22geometry%22%3A%7B%22type%22%3A%22Polygon%22%2C%22coordinates%22%3A%5B%5B%5B-87.80672550201416%2C41.74262728637672%5D%2C%5B-87.80672550201416%2C41.97582726102573%5D%2C%5B-87.45653629302979%2C41.97582726102573%5D%2C%5B-87.45653629302979%2C41.74262728637672%5D%2C%5B-87.80672550201416%2C41.74262728637672%5D%5D%5D%7D%7D');
+  visit('/discover/aggregate?%AAA');
   andThen(function() {
     assert.equal(currentRouteName(), 'discover.index', 'Page did not return to index. (obs_date__ge)');
   });
