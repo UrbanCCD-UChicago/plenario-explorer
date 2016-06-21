f2400cffda58c276005d56e47a6cf41ef1e2b178
George Moe <george@george.moe>
2016-06-21 15:42:39 -0500

    Added show/hide to datatable, which has been moved to the bottom of the page.

--- app/components/data-table.js
+++ app/components/data-table.js
@@ -6,5 +6,23 @@ export default Ember.Component.extend({
     this._super(...arguments);
     this.set('columns', Object.keys(this.get('events')[0]));
     this.set('columnNames', Object.keys(this.get('events')[0]).map(v => humanizeName(v)));
+  },
+
+  showTable: false,
+
+  actions: {
+
+    showDatasetPreview() {
+      this.set('showTable', true);
+      console.log($('#container').outerHeight(true));
+      $('#container-container').animate({height: $('#container').outerHeight(true)}, 500);
+      $('html,body').animate({scrollTop: $(document).height()}, 500);
+
+    },
+    hideDatasetPreview() {
+      this.set('showTable', false);
+      $('#container-container').animate({height: '0vh'}, 500);
+    },
+
   }
 });
