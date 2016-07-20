e9ba87719c2f5eb22482195c3629d0fec4b70d2a
George Moe <george@george.moe>
2016-07-20 15:22:43 -0500

    Narrowed Global Error scope to only handle malformed URIs.

--- app/app.js
+++ app/app.js
@@ -25,10 +25,14 @@ export default App;
 
 Ember.onerror = function (error) {
   console.log('GLOBAL ERROR:', error.message);
-  window.location = `/explore/error/${error.message}`;  
+  if(error.message.toLowerCase().indexOf("uri")>-1) {
+    window.location = `/explore/error/${error.message}`;
+  }
 	//TODO: Quick and dirty redirect...
 	//This will route to, in any case, to either a 404 or error page
 	//and will nicely deposit the user back to the explore index.
 	//as long as the root is /explore.
-        //TODO: Need to figure out how to transition to route from here.
+  //TODO: Need to figure out how to transition to route from here.
+  //TODO: One possible solution is to catch the error in the router
+  //and modify the target route.
 };
