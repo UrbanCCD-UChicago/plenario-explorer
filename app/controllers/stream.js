990adf5114f75c71e7db606bd1f20dfed8b64991
Will Engler <Engler.Will@gmail.com>
2016-08-22 12:12:29 -0500

    Keep track of slected node in controller

--- app/controllers/stream.js
+++ app/controllers/stream.js
@@ -1,9 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  selectedNode: null,
+
   actions: {
-    onClick(marker) {
-      console.log(marker);
+    onClick(event) {
+      let {lat, lng} = event.latlng;
+      const coordinatesKey = JSON.stringify([lng, lat]);
+      this.set('selectedNode',
+        this.get('coordinatesToNodeId').get(coordinatesKey)
+      );
     }
+  },
+
+  // Need to create lookup table from lon-lat to node ids
+  modelArrived: Ember.observer('model', function() {
+    const nodeMapping = this.createNodeMapping(this.get('model').nodes);
+    this.set('coordinatesToNodeId', nodeMapping);
+  }),
+
+  createNodeMapping(nodes) {
+    const coordsToNodes = nodes.map(node => {
+        const {geometry: {coordinates: coordinates}} = node;
+        return [JSON.stringify(coordinates), node];
+      });
+    return new Map(coordsToNodes);
   }
+
 });
