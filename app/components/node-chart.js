026af44b386df81f74e805bd461680ccd3455614
Will Engler <Engler.Will@gmail.com>
2016-08-22 12:30:48 -0500

    Rename chart component

--- app/components/node-chart.js
+++ app/components/node-chart.js
@@ -2,19 +2,9 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Component.extend({
-    /**
-     * So what's the issue?
-     *
-     * Updating my data attribute doesn't trigger any changes
-     * to the live-chart component!
-     */
-
     io: Ember.inject.service('socket-io'),
     serverUrl: 'localhost:8081',
 
-
-    // Node identifier? Or node object?
-    selectedNode: null,
     // How to model incoming sensor data?
     streamingObservations: Ember.A([]),
 
@@ -33,14 +23,5 @@ export default Ember.Component.extend({
 
       socket.on('data', appendObservations, this);
 
-    },
-
-    actions: {
-        connect() {
-        },
-
-        disconnect() {
-        }
-    },
-
+    }
 });
