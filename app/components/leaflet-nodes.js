a67b458536e5abb6579934c3f33b83822f7dd128
Benjamin Lodi <brlodi@uchicago.edu>
2017-05-17 10:22:58 -0500

    Revert "Cleanup unused components"
    This reverts commit 8d607e8223531a39f772a869bf2bd47a0b1f9271.

---
+++ app/components/leaflet-nodes.js
@@ -0,0 +1,81 @@
+import Ember from 'ember';
+import LeafletBase from './leaflet-base';
+/* global L */
+
+export default LeafletBase.extend({
+
+  classNames: ['leaflet-nodes'],
+
+  // Alternate icon color to signal selection
+  SelectedIcon: L.Icon.extend({
+    options: {
+      iconUrl: 'https://s3.amazonaws.com/ember-dev/assets/images/marker-red.png',
+      iconRetinaUrl: 'https://s3.amazonaws.com/ember-dev/assets/images/marker-red-2x.png'
+    }
+  }),
+
+  DefaultIcon: L.Icon.extend({
+    options: {
+      iconUrl: 'https://s3.amazonaws.com/ember-dev/assets/images/marker-icon.png',
+      iconRetinaUrl: 'https://s3.amazonaws.com/ember-dev/assets/images/marker-icon-2x.png'
+    }
+  }),
+
+  selectionChanged: Ember.observer('selectedNode', function() {
+    this.updateHighlight();
+  }),
+
+  setUpMap() {
+    this._super(...arguments);
+    const SelectedIcon = this.get('SelectedIcon');
+    const selIcon = new SelectedIcon();
+    this.set('selectedIcon', selIcon);
+    const DefaultIcon = this.get('DefaultIcon');
+    const defaultIcon = new DefaultIcon();
+    this.set('defaultIcon', defaultIcon);
+    this.updateHighlight();
+  },
+
+  updateHighlight() {
+    const selected = this.get('selectedNode');
+    console.log(selected);
+    if (!selected) {return;}
+
+    // Clear highlight from previously selected node
+    const lastSelected = this.get('lastSelected');
+    if (lastSelected) {
+      this.setHighlight(this.get('lastSelected'), this.get('defaultIcon'));
+    }
+
+    // Highlight the selected node
+    this.setHighlight(selected, this.get('selectedIcon'));
+
+    // Remember which node was just selected, so we can clear it later
+    this.set('lastSelected', selected);
+  },
+
+  setHighlight(nodeId, icon) {
+    const marker = this.get('nodeIdToMarker').get(nodeId);
+    marker.setIcon(icon);
+  },
+
+  addLayer() {
+    const selectionAction = this.get('selectionAction');
+    const nodeIdToMarker = new Map();
+    const addSelectionListeners = function(feature, marker) {
+      const nodeId = feature.properties.id;
+      nodeIdToMarker.set(nodeId, marker);
+      marker.on(
+        {
+          click: () => {selectionAction(nodeId);}
+        });
+    };
+
+    this.set('nodeIdToMarker', nodeIdToMarker);
+    const layer = L.geoJson(
+      this.get('geoJSON'), {onEachFeature: addSelectionListeners}
+    );
+    this.set('layer', layer);
+  }
+
+});
