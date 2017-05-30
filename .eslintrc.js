94323b478253c9ebc8cd6d5d95247a55d91713ed
Benjamin Lodi <brlodi@uchicago.edu>
2017-05-30 16:54:32 -0500

    Yet more ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,11 +14,8 @@ module.exports = {
     jquery: true,
     qunit: true
   },
-  settings: {
-    'import/core-modules': ['ember']
-  },
   globals: {
-    // Browser-default, JQuery and QUnit globals are set by the "env" field above
+    // Browser, JQuery and QUnit globals are set by the "env" field above
 
     // Mirage
     server: true,
@@ -27,5 +24,40 @@ module.exports = {
     L: true
   },
   rules: {
+    // These eslint-plugin-import rules don't play well with Ember
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+
+    // Ember uses this._super and similar constructions
+    'no-underscore-dangle': ['error', { 'allowAfterThis': true }],
+
+    // Anonymous functions are idiomatic in Ember.computed properties
+    'func-names': 'off',
+
+    // Conflicts with the Ember `this._super(...arguments)` idiom
+    // TODO: move all our init() calls to init(...args)?
+    'prefer-rest-params': 'off',
+
+    // AirBnB disallows all loops anyway, so they don't have this rule
+    // relaxed for for-loop declarations. We do use some loops.
+    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
+
+    // AirBnB disallows loops, preferring functional programming paradigms.
+    // We allow them. TODO: stop allowing them
+    'no-restricted-syntax': 'off',
+
+    // Trailing commas in function parameters is not supported in ES2016
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+    }],
+
+    // Functions are hoisted anyway, and it looks nicer to have utility
+    // functions at the bottom instead of clutting the top of the file
+    'no-use-before-define': ['error', { 'functions': false }]
   }
 };
--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -15,9 +15,6 @@ module.exports = {
     qunit: true,
     embertest: true
   },
-  settings: {
-    'import/core-modules': ['ember']
-  },
   globals: {
     // Browser-default, JQuery and QUnit globals are set by the "env" field above
 
@@ -28,5 +25,40 @@ module.exports = {
     L: true
   },
   rules: {
+    // These eslint-plugin-import rules don't play well with Ember
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+
+    // Ember uses this._super and similar constructions
+    'no-underscore-dangle': ['error', { 'allowAfterThis': true }],
+
+    // Anonymous functions are idiomatic in Ember.computed properties
+    'func-names': 'off',
+
+    // Conflicts with the Ember `this._super(...arguments)` idiom
+    // TODO: move all our init() calls to init(...args)?
+    'prefer-rest-params': 'off',
+
+    // AirBnB disallows all loops anyway, so they don't have this rule
+    // relaxed for for-loop declarations. We do use some loops.
+    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
+
+    // AirBnB disallows loops, preferring functional programming paradigms.
+    // We allow them. TODO: stop allowing them
+    'no-restricted-syntax': 'off',
+
+    // Trailing commas in function parameters is not supported in ES2016
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+    }],
+
+    // Functions are hoisted anyway, and it looks nicer to have utility
+    // functions at the bottom instead of clutting the top of the file
+    'no-use-before-define': ['error', { 'functions': false }]
   }
 };
