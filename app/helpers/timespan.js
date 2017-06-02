ba1d44560abedec58bc3454b0a66b4ad265fad57
Benjamin Lodi <brlodi@uchicago.edu>
2017-06-02 15:25:26 -0500

    Backport timespan fix from integrated-dash

--- app/helpers/timespan.js
+++ app/helpers/timespan.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export function timespan(params) {
-  const { start, end } = params;
+  const [start, end] = params;
   // true parameter sets return format to
   // "5 days" instead of "5 days ago"
   return moment(start).from(moment(end), true);
