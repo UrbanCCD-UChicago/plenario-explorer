2a10be0eaff7e93491b2694f9626b3fc86c19368
Benjamin Lodi <brlodi@uchicago.edu>
2017-06-13 16:53:45 -0500

    Work on making search widget functional

--- app/components/search-widget.js
+++ app/components/search-widget.js
@@ -1,4 +1,54 @@
 import Ember from 'ember';
+import moment from 'npm:moment';
 
 export default Ember.Component.extend({
+
+
+  aggregationOptions: ['hour', 'day', 'week'],
+
+  startDateAsString: Ember.computed('startDate', {
+    get(key) {
+      return this.get('startDate').format('YYYY-MM-DD');
+    },
+    set(key, value) {
+      const momentDate = moment(value);
+      if (momentDate.isValid()) {
+        this.set('startDate', momentDate);
+        return value;
+      }
+      return false;
+    }
+  }),
+
+  endDateAsString: Ember.computed('endDate', {
+    get(key) {
+      return this.get('endDate').format('YYYY-MM-DD');
+    },
+    set(key, value) {
+      const momentDate = moment(value);
+      if (momentDate.isValid()) {
+        this.set('endDate', momentDate);
+        return value;
+      }
+      return false;
+    }
+  }),
+
+  actions: {
+    resetForm() {
+      this.setupInitialValues();
+    }
+  },
+
+  init() {
+    this._super(...arguments);
+    this.setupInitialValues();
+  },
+
+  setupInitialValues() {
+    this.set('startDate', moment().subtract(7, 'days').startOf('day'));
+    this.set('endDate', moment().endOf('day'));
+    this.set('aggregateBy', 'day');
+  },
+
 });
