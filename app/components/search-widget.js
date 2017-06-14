bcf9fce88c6d5262cd4c9d0e05d0580214ee9ee5
Benjamin Lodi <brlodi@uchicago.edu>
2017-06-14 14:35:26 -0500

    Wrap up initial implementation of search widget

--- app/components/search-widget.js
+++ app/components/search-widget.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import moment from 'npm:moment';
+import bboxPolygon from 'npm:@turf/bbox-polygon';
 
 export default Ember.Component.extend({
 
@@ -14,7 +15,7 @@ export default Ember.Component.extend({
       const momentDate = moment(value);
       if (momentDate.isValid()) {
         this.set('startDate', momentDate);
-        return value;
+        return momentDate.format();
       }
       return false;
     }
@@ -28,15 +29,46 @@ export default Ember.Component.extend({
       const momentDate = moment(value);
       if (momentDate.isValid()) {
         this.set('endDate', momentDate);
-        return value;
+        return momentDate.format();
       }
       return false;
     }
   }),
 
+  queryAreaAsGeoJson: Ember.computed('currentMapBounds', 'userShapeGeoJson', function () {
+    if (this.get('userShapeGeoJson') !== null && this.get('userShapeGeoJson') !== undefined) {
+      return this.get('userShapeGeoJson');
+    } else if (this.get('currentMapBounds')) {
+      return bboxPolygon(
+        this.get('currentMapBounds').toBBoxString()
+          .split(',')
+          .map(coord => Number(coord))
+      );
+    } else {
+      return null;
+    }
+  }),
+
+  formValues: Ember.computed('startDate', 'endDate', 'aggregateBy', 'queryAreaAsGeoJson', function () {
+    const startDateString = this.get('startDate').format();
+    const endDateString = this.get('endDate').format();
+    const aggregateBy = this.get('aggregateBy');
+    const queryAreaAsGeoJsonString = JSON.stringify(this.get('queryAreaAsGeoJson'));
+    return { startDateString, endDateString, aggregateBy, queryAreaAsGeoJsonString };
+  }),
+
   actions: {
+    submitForm() {
+      this.get('onSubmit')(this.get('formValues'));
+    },
     resetForm() {
       this.setupInitialValues();
+    },
+    mapDidMove(event) {
+      this.set('currentMapBounds', event.target.getBounds());
+    },
+    userDidDrawShape(event) {
+      this.set('userShapeGeoJson', event.layer.toGeoJSON());
     }
   },
 
@@ -49,6 +81,7 @@ export default Ember.Component.extend({
     this.set('startDate', moment().subtract(7, 'days').startOf('day'));
     this.set('endDate', moment().endOf('day'));
     this.set('aggregateBy', 'day');
+    this.set('userShapeGeoJson', null);
   },
 
 });
--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  actions: {
+    doSearch(searchParams) {
+      console.log(searchParams);
+    },
+  }
+
 });
