946d5d74f3f366d2a6c541d7be9be5e3ebb8920c
Benjamin Lodi <brlodi@uchicago.edu>
2017-06-20 21:27:53 -0500

    Add ember-light-table, update ember-leaflet-draw
    The ember-leaflet-draw update removes its dependence on the leaflet-draw
    bower package, so this commit also KILLS BOWER WITH FIRE 🔥🔥☠️🔥🔥

---
+++ app/breakpoints.js
@@ -0,0 +1,8 @@
+export default {
+  // Duplicate the bootstrap breakpoints
+  xs: '(max-width: 575px)',
+  sm: '(min-width: 576px)',
+  md: '(min-width: 768px)',
+  lg: '(min-width: 992px)',
+  xl: '(min-width: 1200px)',
+};
