aab5de7ea04d16cc667c8650f22281c1313b6b80
Benjamin Lodi <brlodi@uchicago.edu>
2017-06-22 16:13:11 -0500

    Implement results table filtering by string input

--- app/controllers/search/results.js
+++ app/controllers/search/results.js
@@ -6,14 +6,33 @@ export default Ember.Controller.extend({
 
   queryParameters: ['startDate', 'endDate', 'withinArea'],
 
+  filterNodeFeaturePseudoDatasetsBy: '',
+  sortNodeFeaturePseudoDatasetsBy: ['name'],
+  filterOpenDataProviderDatasetsBy: '',
+  sortOpenDataProviderDatasetsBy: ['attribution', 'human_name'],
+
   nodeFeaturePseudoDatasets: Ember.computed.alias('model.features'),
   openDataProviderDatasets: Ember.computed.uniq('model.events', 'model.shapes'),
 
-  sensorDataTable: Ember.computed('nodeFeaturePseudoDatasets', function () {
+  nodeFeaturePseudoDatasetFilterChanged: Ember.observer(
+    'filterNodeFeaturePseudoDatasetsBy',
+    function () {
+      Ember.run.once(this, 'filterNodeFeatureTableRows');
+    }
+  ),
+  openDataProviderDatasetFilterChanged: Ember.observer(
+    'filterOpenDataProviderDatasetsBy',
+    function () {
+      Ember.run.once(this, 'filterOpenDataProviderTableRows');
+    }
+  ),
+
+  nodeFeatureDataTable: Ember.computed('nodeFeaturePseudoDatasets', function () {
     return new Table(
       [
         {
           cellComponent: 'lt-cell-checkbox',
+          sortable: false,
           width: '2em',
           align: 'center',
         },
@@ -23,7 +42,7 @@ export default Ember.Controller.extend({
           format: this.humanize,
         },
         {
-          label: 'Properties',
+          label: 'Available Subtypes',
           valuePath: 'properties',
           format: this.formatFeatureProperties,
         },
@@ -37,21 +56,31 @@ export default Ember.Controller.extend({
       [
         {
           cellComponent: 'lt-cell-checkbox',
+          sortable: false,
           width: '2em',
           align: 'center',
-        },
-        {
-          label: 'Source',
-          valuePath: 'attribution',
         },
         {
           label: 'Dataset Name',
           valuePath: 'human_name',
         },
+        {
+          label: 'Source',
+          valuePath: 'attribution',
+        },
       ],
       this.get('openDataProviderDatasets')
     );
   }),
+
+  userHasMadeASelection: Ember.computed.or(
+    'nodeFeatureDataTable.selectedRows.length',
+    'openDataTable.selectedRows.length'
+  ),
+
+  actions: {
+
+  },
 
   humanize(string) {
     return _.startCase(_.replace(string, /_/, ' '));
@@ -61,6 +90,39 @@ export default Ember.Controller.extend({
     return properties.map(prop => (
       prop.common_name ? _.split(prop.common_name, ':').slice(-1) : prop.name
     )).join(', ');
+  },
+
+  filterNodeFeatureTableRows() {
+    const searchString = this.get('filterNodeFeaturePseudoDatasetsBy').toLowerCase();
+    this.get('nodeFeatureDataTable').rows.forEach((row) => {
+      if (searchString === '') {
+        Ember.set(row, 'hidden', false); // Unhide everything if there's no filter active
+        return;
+      }
+      const pdName = this.humanize(row.content.name).toLowerCase();
+      const pdProps = this.formatFeatureProperties(row.content.properties).toLowerCase();
+      if (pdName.includes(searchString) || pdProps.includes(searchString)) {
+        Ember.set(row, 'hidden', false);
+      } else {
+        Ember.set(row, 'hidden', true);
+      }
+    });
+  },
+
+  filterOpenDataProviderTableRows() {
+    const searchString = this.get('filterOpenDataProviderDatasetsBy').toLowerCase();
+    this.get('openDataTable').rows.forEach((row) => {
+      if (searchString === '') {
+        Ember.set(row, 'hidden', false); // Unhide everything if there's no filter active
+      }
+      const dsName = row.content.human_name.toLowerCase();
+      const dsSource = row.content.attribution.toLowerCase();
+      if (dsName.includes(searchString) || dsSource.includes(searchString)) {
+        Ember.set(row, 'hidden', false);
+      } else {
+        Ember.set(row, 'hidden', true);
+      }
+    });
   },
 
 
