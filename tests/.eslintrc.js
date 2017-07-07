69f75a1e591165ea594125017f8c76a3878e09a8
Benjamin Lodi <brlodi@uchicago.edu>
2017-07-07 16:15:23 -0500

    Update ESLint settings for tests

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -7,5 +7,9 @@ module.exports = {
     // better and they apparently give no $&*#s about best practices. ESLint (correctly) interprets
     // those chains as property access expressions, so we have to disable the whole darn rule
     'no-unused-expressions': 'off',
+
+    // ESLint only checks the outermost function for occurrences of 'this' to ignore this rule,
+    // which doesn't work with Mocha's dynamic test generation paradigm
+    'prefer-arrow-callback': 'off',
   }
 };
