57884ed2b540b2fa3ac0f734ad5777f5cc80aee5
Benjamin Lodi <brlodi@uchicago.edu>
2017-08-03 16:56:34 -0500

    A couple ESLint fixes

--- tests/integration/components/download-menu-test.js
+++ tests/integration/components/download-menu-test.js
@@ -3,12 +3,12 @@ import { describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
-describe('Integration | Component | download menu', function() {
+describe('Integration | Component | download menu', function () {
   setupComponentTest('download-menu', {
-    integration: true
+    integration: true,
   });
 
-  it('renders', function() {
+  it('renders', function () {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
     // Template block usage:
--- tests/unit/helpers/format-date-test.js
+++ tests/unit/helpers/format-date-test.js
@@ -2,10 +2,10 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { formatDate } from 'plenario-explorer/helpers/format-date';
 
-describe('Unit | Helper | format date', function() {
+describe('Unit | Helper | format date', function () {
   // Replace this with your real tests.
-  it('works', function() {
-    let result = formatDate(42);
+  it('works', function () {
+    const result = formatDate(42);
     expect(result).to.be.ok;
   });
 });
